db1677f49bc959787bd37f8e66be66cad5c539e1
cicdguy <26552821+cicdguy@users.noreply.github.com>
2023-10-31 12:02:01 -0500

    Update search indexes

--- core.R
+++ core.R
@@ -1,35 +1,96 @@
 #!/usr/bin/env Rscript
 
+# Set vars and constants
 start_tag <- "<!-- start dropdown for versions -->"
 end_tag <- "<!-- end dropdown for versions -->"
 
-prepare_dropdown_button <- function(refs_to_list = paste(
-                                      "^main$",
-                                      "^devel$",
-                                      "^pre-release$",
-                                      "^latest-tag$",
-                                      "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
-                                      sep = "|"
-                                    ),
-                                    refs_order = c(
-                                      "devel",
-                                      "pre-release",
-                                      "main",
-                                      "latest-tag"
-                                    ),
-                                    version_tab = "") {
+repo_name <-
+  gsub(
+    paste0(Sys.getenv("GITHUB_REPOSITORY_OWNER"), "/"),
+    "",
+    Sys.getenv("GITHUB_REPOSITORY")
+  )
+base_url <- paste0(
+  "https://",
+  Sys.getenv("GITHUB_REPOSITORY_OWNER"),
+  ".github.io/",
+  repo_name
+)
+search_index_file <- "search.json"
 
-  conf <- eval(parse(text = version_tab))
-
+filter_versions <- function(refs_to_list = paste(
+                              "^main$",
+                              "^devel$",
+                              "^pre-release$",
+                              "^latest-tag$",
+                              "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
+                              sep = "|"
+                            )) {
   # List and sort versions
-  versions <- sort(list.dirs(
-    path = ".",
-    recursive = FALSE,
-    full.names = FALSE
-  ), decreasing = TRUE)
+  versions <- sort(
+    list.dirs(
+      path = ".",
+      recursive = FALSE,
+      full.names = FALSE
+    ),
+    decreasing = TRUE
+  )
 
   # Filter versions according to refs_to_list
-  versions <- versions[grep(refs_to_list, versions)]
+  return(versions[grep(refs_to_list, versions)])
+}
+
+update_search_indexes <- function(refs_to_list = paste(
+                                    "^main$",
+                                    "^devel$",
+                                    "^pre-release$",
+                                    "^latest-tag$",
+                                    "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
+                                    sep = "|"
+                                  )) {
+  # Filter versions according to refs_to_list
+  versions <- filter_versions(refs_to_list)
+
+  oldwd <- getwd()
+  for (version in versions) {
+    # Navigate to the ref
+    setwd(version)
+    # Update the indexes
+    if (file.exists(search_index_file)) {
+      search_index <- readLines(search_index_file)
+      updated_index <- gsub(
+        paste0(base_url, "(?!/", version, ")"),
+        paste0(base_url, "/", version),
+        search_index,
+        perl = TRUE
+      )
+      writeLines(updated_index, search_index_file)
+    }
+    # Reset working directory
+    setwd(oldwd)
+  }
+}
+
+prepare_dropdown_button <- function(
+    refs_to_list = paste(
+      "^main$",
+      "^devel$",
+      "^pre-release$",
+      "^latest-tag$",
+      "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
+      sep = "|"
+    ),
+    refs_order = c(
+      "devel",
+      "pre-release",
+      "main",
+      "latest-tag"
+    ),
+    version_tab = "") {
+  conf <- eval(parse(text = version_tab))
+
+  # Filter versions according to refs_to_list
+  versions <- filter_versions(refs_to_list)
   output <- c()
 
   # Append versions to output vector according to
@@ -41,10 +102,11 @@ prepare_dropdown_button <- function(refs_to_list = paste(
     }
   }
 
-  other_versions <- versions[!grepl(
-    paste0(refs_order, collapse = "|"),
-    versions
-  )]
+  other_versions <-
+    versions[!grepl(
+      paste0(refs_order, collapse = "|"),
+      versions
+    )]
 
   # Append versions other than ones in refs_order
   # at the bottom of drop-down list.
@@ -52,34 +114,44 @@ prepare_dropdown_button <- function(refs_to_list = paste(
   # E.g. v0.1.1 should not be before v0.1.10
   versions <- c(
     output,
-    rev(other_versions[
-      order(nchar(other_versions),
-      other_versions)
-    ])
+    rev(other_versions[order(
+      nchar(other_versions),
+      other_versions
+    )])
   )
   print(paste0("Version order in drop-down: ", versions))
 
-  text <- sapply(versions, FUN = function(x) {
-    text <- conf$config$text[[x]]
-    if (is.null(text)) x else text
-  }, simplify = TRUE)
+  text <- sapply(
+    versions,
+    FUN = function(x) {
+      text <- conf$config$text[[x]]
+      if (is.null(text)) {
+        x
+      } else {
+        text
+      }
+    },
+    simplify = TRUE
+  )
 
-  tooltip <- sapply(versions, FUN = function(x) {
-    text <- conf$config$tooltip[[x]]
-    if (is.null(text)) "" else text
-  }, simplify = TRUE)
+  tooltip <- sapply(
+    versions,
+    FUN = function(x) {
+      text <- conf$config$tooltip[[x]]
+      if (is.null(text)) {
+        ""
+      } else {
+        text
+      }
+    },
+    simplify = TRUE
+  )
 
   menu_items <- paste0(
     '<a class="dropdown-item" data-toggle="tooltip" title="',
     tooltip,
-    '" href="https://',
-    Sys.getenv("GITHUB_REPOSITORY_OWNER"),
-    ".github.io/",
-    gsub(
-      paste0(Sys.getenv("GITHUB_REPOSITORY_OWNER"), "/"),
-      "",
-      Sys.getenv("GITHUB_REPOSITORY")
-    ),
+    '" href="',
+    base_url,
     "/",
     versions,
     '">',
@@ -105,25 +177,25 @@ prepare_dropdown_button <- function(refs_to_list = paste(
   return(button)
 }
 
-update_content <- function(refs_to_list = paste(
-                             "^main$",
-                             "^devel$",
-                             "^pre-release$",
-                             "^latest-tag$",
-                             "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
-                             sep = "|"
-                           ),
-                           insert_after_section = "Changelog",
-                           refs_order = c(
-                            "devel",
-                            "pre-release",
-                            "main",
-                            "latest-tag"
-                          ),
-                           version_tab = "") {
-  dropdown_button <- prepare_dropdown_button(
-    refs_to_list, refs_order, version_tab
-  )
+update_content <- function(
+    refs_to_list = paste(
+      "^main$",
+      "^devel$",
+      "^pre-release$",
+      "^latest-tag$",
+      "^v([0-9]+\\.)?([0-9]+\\.)?([0-9]+)$",
+      sep = "|"
+    ),
+    insert_after_section = "Changelog",
+    refs_order = c(
+      "devel",
+      "pre-release",
+      "main",
+      "latest-tag"
+    ),
+    version_tab = "") {
+  dropdown_button <-
+    prepare_dropdown_button(refs_to_list, refs_order, version_tab)
 
   html_files <- list.files(
     path = ".",
@@ -133,21 +205,22 @@ update_content <- function(refs_to_list = paste(
     full.names = TRUE
   )
 
-  insert_after <- paste0('index.html">', insert_after_section, "</a>")
+  insert_after <-
+    paste0('index.html">', insert_after_section, "</a>")
 
   for (f in html_files) {
     html_content <- readLines(f)
     # Replace previous instances
-    drowdown_start_line <- grep(
-      pattern = start_tag, x = html_content
-    )
-    dropdown_end_line <- grep(
-      pattern = end_tag, x = html_content
-    )
-    if (length(drowdown_start_line) > 0 && length(dropdown_end_line) > 0) {
-      html_content <- html_content[- (drowdown_start_line:dropdown_end_line)]
+    drowdown_start_line <-
+      grep(pattern = start_tag, x = html_content)
+    dropdown_end_line <- grep(pattern = end_tag, x = html_content)
+    if (length(drowdown_start_line) > 0 &&
+      length(dropdown_end_line) > 0) {
+      html_content <-
+        html_content[-(drowdown_start_line:dropdown_end_line)]
     }
-    insert_line <- grep(pattern = insert_after, x = html_content) + 1
+    insert_line <-
+      grep(pattern = insert_after, x = html_content) + 1
     if (length(insert_line > 0)) {
       html_content <- c(
         html_content[1:insert_line],
@@ -156,14 +229,13 @@ update_content <- function(refs_to_list = paste(
       )
       writeLines(html_content, f)
     } else {
-      message(
-        paste(
-          f,
-          ": Could not find the",
-          insert_after,
-          "tag"
-        )
-      )
+      message(paste(
+        f,
+        ": Could not find the",
+        insert_after,
+        "tag"
+      ))
     }
   }
+  update_search_indexes(refs_to_list)
 }
